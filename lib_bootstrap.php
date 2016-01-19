61480a2725cc6663f7edecfbffbc1f2983137cb6
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2016-01-19 21:52:27 +0100

    fixed path

--- lib_bootstrap.php
+++ lib_bootstrap.php
@@ -4,9 +4,9 @@ class lib_bootstrap extends \LIB\lib_jscss{
     public static function get_class(){
         return self::class;}
     public static function js_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'/bootstrap/lib/js/bootstrap.min.js');}
+        return new \SYSTEM\PLIB('/bootstrap/lib/js/bootstrap.min.js');}
     public static function css_path(){
-        return \SYSTEM\WEBPATH(new \SYSTEM\PLIB(),'/bootstrap/lib/css/bootstrap.min.css');}
+        return new \SYSTEM\PLIB('/bootstrap/lib/css/bootstrap.min.css');}
     public static function version(){
         return '3.3.4';}
 }
